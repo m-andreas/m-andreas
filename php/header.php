43ec1e179df5265c8aa38baeacab6dca0ae12d5a
m-andreas <mar.andreas@yahoo.de>
2015-08-04 00:15:02 +0200

    neue ci eingefügt

---
+++ php/header.php
@@ -0,0 +1,12 @@
+          <div class="masthead clearfix">
+            <div class="inner">
+              <div class="masthead-brand"><img src="img/logo_hexa.png" alt="logo" height="80" width="160">web.wien Ihr Parter für webbasierte Software</div>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="#">Home</a></li>
+                  <li><a href="#">Was wir anbieten</a></li>
+                  <li><a href="#">Kontakt</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
